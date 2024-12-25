2edb89234176920ae963ecfb6dbaeeae07810e20
Hashir Irshad <khanjarquantum@gmail.com>
2024-12-25 16:44:51 +0500

    new commit

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
         remotePatterns: [
             {
                 protocol: "https",
-                hostname: "cdn.sanity.io",
+                domains: ['cdn.sanity.io'],
                 port:"",
         
         }
